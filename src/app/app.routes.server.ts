d5509687907d41fd6d2194b6b66b8589ae1b7ea8
jferruscagonzalez@icloud.com <jferruscagonzalezicloud.com@MacBook-Pro-de-jferruscagonzalezicloudcom.local>
2026-07-28 01:03:32 -0600

    Configurar prerender para Netlify

--- src/app/app.routes.server.ts
+++ src/app/app.routes.server.ts
@@ -2,6 +2,19 @@ import { RenderMode, ServerRoute } from '@angular/ssr';
 
 export const serverRoutes: ServerRoute[] = [
   {
+    path: 'pokemons/:id',
+    renderMode: RenderMode.Prerender,
+    async getPrerenderParams() {
+    return [
+    { id: '1' },
+    { id: '2' },
+    { id: '3' },
+    { id: '4' },
+    { id: '5' },
+    ];
+    }
+  },
+  {
     path: '**',
     renderMode: RenderMode.Prerender,
   },
